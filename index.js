3b3eea2c1191bdd1b57ab73a969c8915908fd160
Oussama <osm2014@hotmail.fr>
2022-01-13 14:58:33 +0100

    created booking model

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const port = 3000
+const express       = require('express')
+const mongoose      = require('mongoose')
+const path          = require('path')
+const port          = 3000
+const app           = express()
+const Booking       =require('./models/booking')
+// connection to mongodb through mongoose
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/booking-app');
+  console.log('connected to mongodb !')
+}
+
+
+// bookings page
+app.get('/bookings', async (req,res) => {
+    const booking = new Booking({title: 'residence omar', price:3000,description:'something', location:'tanger beni makada'})
+    await booking.save()
+    res.send(booking)        
+})
+
+
+// set ejs as view engine
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
 
 
+// route to home
 app.get('/', (req,res) => {
     res.send('welcome to youRent')
 })
 
 
-
+// setup server on port 3000
 app.listen(port, () => {
     console.log(`serving your app on http://localhost:${port}`)
 })
---
+++ models/booking.js
@@ -0,0 +1,21 @@
+const mongoose      = require('mongoose')
+const { Schema }    = mongoose
+
+// createing schema for a booking
+const bookingSchema = Schema({
+    title: String,
+    description: String,
+    price: String,
+    location:String
+})
+
+
+
+// creating model from our schema
+const Booking = mongoose.model('Booking', bookingSchema)
+
+// exporting our model
+module.exports = Booking;
+
+
+
